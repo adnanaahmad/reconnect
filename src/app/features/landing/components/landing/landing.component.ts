1f4e029616b1ba80cd98a28eb367eaa10e4db134
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-01-12 17:49:42 +0500

    sign up ui for buyer made

--- src/app/features/landing/components/landing/landing.component.ts
+++ src/app/features/landing/components/landing/landing.component.ts
@@ -10,6 +10,7 @@ export class LandingComponent implements OnInit, AfterViewInit {
   @ViewChild('card') cardComponent: ElementRef;
   @ViewChild('reconnect') reconnectButton: ElementRef;
   @ViewChild('login') loginButton: ElementRef;
+  @ViewChild('signup') registerButton: ElementRef;
   modal: boolean;
   constructor(private router: Router, private cdr: ChangeDetectorRef) { }
 
@@ -34,18 +35,42 @@ export class LandingComponent implements OnInit, AfterViewInit {
   }
   changeBackground(): void{
     if (this.router.url === '/'){
-      this.modal = false;
-      this.cardComponent.nativeElement.style.opacity = '0';
-      this.reconnectButton.nativeElement.style.color = 'var(--darkGray)';
-      this.loginButton.nativeElement.style.color = 'var(--darkGray)';
+      this.changeBackgroundHelper(
+          false,
+          '0',
+          '0s',
+          'var(--darkGray)',
+          'transparent',
+          'transparent'
+      );
+    } else if (this.router.url === '/login'){
+      this.changeBackgroundHelper(
+          true, '1',
+          '0.4s',
+          'white',
+          'var(--green)',
+          'transparent'
+      );
     } else {
-      this.modal = true;
-      this.cardComponent.nativeElement.style.opacity = '1';
-      this.cardComponent.nativeElement.style.transition = '.4s';
-      this.reconnectButton.nativeElement.style.color = 'white';
-      this.loginButton.nativeElement.style.color = 'white';
+      this.changeBackgroundHelper(
+          true,
+          '1',
+          '0.4s',
+          'white',
+          'transparent',
+          'var(--green)'
+      );
     }
-    //this.cdr.detectChanges();
+  }
+  changeBackgroundHelper(modal, opacity, transition, color, loginBackground, registerBackground): void{
+    this.modal = modal;
+    this.cardComponent.nativeElement.style.opacity = opacity;
+    this.cardComponent.nativeElement.style.transition = transition;
+    this.reconnectButton.nativeElement.style.color = color;
+    this.loginButton.nativeElement.style.color = color;
+    this.registerButton.nativeElement.style.color = color;
+    this.loginButton.nativeElement.style.background = loginBackground;
+    this.registerButton.nativeElement.style.background = registerBackground;
   }
   showModal(): void{
     this.modal = this.router.url !== '/';
--- src/app/features/landing/components/registration/registration.component.ts
+++ src/app/features/landing/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+  screen = {one: true, two: false, three: false, four: false, five: false};
+  professional: any;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.professional = [
+        {image: 'https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg', name: 'Daniel Kevin', role: 'Real Estate Agent', number: '+923 24 4 1276'},
+        {image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHPzYd8Zq8A3IXMaeSeTyfaSTce2CLi0NZ-Q&usqp=CAU', name: 'Tom Holland', role: 'Attorney', number: '+923 24 4 1276'},
+        {image: 'https://alicepropertymanagement.com/images/temp-profile.jpg', name: 'Sean Paul', role: 'Lender', number: '+923 24 4 1276'},
+    ];
   }
+  selectProfessional(): void{
 
+  }
 }
