e854b2b705778c47041c65d870ddc2eaeb5cc374
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-01-13 19:00:56 +0500

    login made dynamic

--- src/app/features/landing/components/login/login.component.ts
+++ src/app/features/landing/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {LoginModel} from '../../models/login.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  login: LoginModel = {} as LoginModel;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.login.form = this.fb.group({
+      email: [null, [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: [null, Validators.required]
+    });
   }
-
+  onSubmit(): void{
+    console.log(this.login.form.value);
+  }
 }
