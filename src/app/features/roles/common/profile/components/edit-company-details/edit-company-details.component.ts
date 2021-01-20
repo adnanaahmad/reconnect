bae8dab17059e44e892537d6d162541294a8b2af
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-01-20 18:38:52 +0500

    reactive form added for company detail

--- src/app/features/roles/common/profile/components/edit-company-details/edit-company-details.component.ts
+++ src/app/features/roles/common/profile/components/edit-company-details/edit-company-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-company-details',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-company-details.component.scss']
 })
 export class EditCompanyDetailsComponent implements OnInit {
-
-  constructor() { }
+  companyDetails: any;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.companyDetails = this.fb.group({
+      image: [null, Validators.required],
+      companyName: [null, Validators.required],
+      license: [null, Validators.required],
+      officePhone: [null, Validators.required],
+      faxPhone: [null, Validators.required],
+      street: [null, Validators.required],
+      city: [null, Validators.required],
+      state: [null, Validators.required],
+      zipCode: [null, Validators.required],
+    });
   }
-
+  onSubmit(): void{
+    console.log(this.companyDetails.value);
+  }
 }
