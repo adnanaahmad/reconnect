fe51bcf7acb05169af4ef1466b992521992a5811
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-02-19 16:52:45 +0500

    if user logs out from one tab, tries to navigate to some other feature in the other tab, user will be re directed to login

--- src/app/core/roleGuard/role.guard.ts
+++ src/app/core/roleGuard/role.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanLoad, Route, UrlSegment, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {ConstantService} from '../constant/constant.service';
 import {StoreService} from '../store/store.service';
@@ -8,10 +8,14 @@ import {StoreService} from '../store/store.service';
   providedIn: 'root'
 })
 export class RoleGuard implements CanLoad {
-  constructor(private constant: ConstantService, private store: StoreService) {}
+  constructor(private constant: ConstantService, private store: StoreService, private router: Router) {}
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!localStorage.getItem('token')) {
+      this.router.navigateByUrl('/login').then();
+      return false;
+    }
     const role = JSON.parse(localStorage.getItem('user')).role;
     return this.constant.permission[role][route.path];
   }
