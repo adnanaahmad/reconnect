d6b17f33ab17ddf6e3d9e6a4bf0817480d9fcea0
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-03-11 17:03:59 +0500

    share property members ui set to wrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 const serverUrl = 'https://narsunprojects.com:5000';
 //const serverUrl = 'http://192.168.0.27:5000';
-const clientUrl = 'http://localhost:4200/#';
+const clientUrl = 'https://reconnect-narsun.web.app';
 export const environment = {
   production: true,
   apiUrl: `${serverUrl}/api`,
