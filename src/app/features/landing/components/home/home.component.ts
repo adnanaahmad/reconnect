14d11b059ddad87813334356f2196dbc94b5b028
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-05-10 12:07:07 +0500

    REC-418 REC-420 bugs resolved

--- src/app/features/landing/components/home/home.component.ts
+++ src/app/features/landing/components/home/home.component.ts
@@ -9,15 +9,18 @@ import {take} from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   homes: Array<any>;
-  houses: Array<any>;
-  team: Array<string>;
+  popularPlaces: Array<any>;
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
-    this.houses = ['https://images.adsttc.com/media/images/5be9/fd5c/08a5/e5a5/8c00/008f/large_jpg/CARLES_FAUS_ARQUITECTURA_-_CARMEN_HOUSE_(2).jpg?1542061390',
-      'https://nimvo.com/wp-content/uploads/2018/04/Modern-House.jpg',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnm8ECMJaAQu_rUUfaYZy-FQnl4XQbgsyLcA&usqp=CAU',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTahA0LYlzFA-K_B3pu8ruheyd1STRBMXTxbg&usqp=CAU'];
+    this.popularPlaces = [
+      {url: '/assets/landing/boston.png', name: 'Boston, MA'},
+      {url: '/assets/landing/capeCod.png', name: 'Cape Cod, MA'},
+      {url: '/assets/landing/cambridge.png', name: 'Cambridge, MA'},
+      {url: '/assets/landing/salem.png', name: 'Salem, MA'},
+      {url: '/assets/landing/worcester.png', name: 'Worcester, MA'},
+      {url: '/assets/landing/springfield.png', name: 'Springfield, MA'},
+    ];
     this.getFeaturedHomes();
   }
   getFeaturedHomes(): void{
