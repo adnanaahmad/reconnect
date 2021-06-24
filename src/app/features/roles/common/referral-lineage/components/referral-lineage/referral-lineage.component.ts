f1bd727c5b0f82641700abab00061515c5857219
adnanahmad95 <adnan.ahmad@narsun.pk>
2021-06-24 10:57:11 +0500

    removed yfiles

--- src/app/features/roles/common/referral-lineage/components/referral-lineage/referral-lineage.component.ts
+++ src/app/features/roles/common/referral-lineage/components/referral-lineage/referral-lineage.component.ts
@@ -1,150 +1,145 @@
-import {
-  AfterViewInit,
-  ApplicationRef,
-  Component,
-  ComponentFactoryResolver,
-  Injector,
-  NgZone, OnInit,
-  ViewChild
-} from '@angular/core';
-import {
-  IArrow,
-  ICommand,
-  IGraph,
-  PolylineEdgeStyle,
-  OrganicEdgeRouter,
-  Size,
-  TreeLayout,
-  TreeReductionStage,
-  INode, NodeStylePortStyleAdapter, Point, ShapeNodeStyle, GraphTransformer, DefaultNodePlacer, Class, LayoutExecutor
-} from 'yfiles';
-import {EDGE_DATA, NODE_DATA} from '../../../../../../../../yFiles/demos-js/toolkit/angular/src/app/data';
-import {GraphComponentComponent} from '../graph-component/graph-component.component';
-import {NodeComponentStyle} from '../../nodeComponentStyle';
+import {Component, OnInit} from '@angular/core';
 import { BuyerModel } from '../../models/referral-lineage.model';
-Class.ensure(LayoutExecutor)
+const NODE_DATA =
+    {
+      id: 1,
+      name: 'Eric Joplin',
+      email: 'ejoplin@yoyodyne.com',
+      phone: '555-0100',
+      dealStatus: 'Real Estate Agent',
+      photo: 'https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg',
+      children: [{
+
+        id: 2,
+        name: 'Gary Roberts',
+        email: 'groberts@yoyodyne.com',
+        phone: '555-0100',
+        dealStatus: 'Under Agreement',
+        photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+        children: [{
+          id: 3,
+          name: 'Alexander Burns',
+          email: 'aburns@yoyodyne.com',
+          phone: '555-0102',
+          dealStatus: 'Closed',
+          photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+          children: []
+        },
+          {
+            id: 4,
+            name: 'Linda Newland',
+            email: 'lnewland@yoyodyne.com',
+            phone: '555-0112',
+            dealStatus: 'Under Agreement',
+            photo: 'https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg',
+            children: []
+          }]
+
+      },
+        {
+          id: 5,
+          name: 'Amy Kain',
+          email: 'akain@yoyodyne.com',
+          phone: '555-0106',
+          dealStatus: 'Closed',
+          photo: 'https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg',
+          children: [{
+            id: 6,
+            name: 'Dorothy Turner',
+            email: 'dturner@yoyodyne.com',
+            phone: '555-0108',
+            dealStatus: 'Closed',
+            photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+            children: [
+              {
+                id: 11,
+                name: 'Linda Newland',
+                email: 'lnewland@yoyodyne.com',
+                phone: '555-0112',
+                dealStatus: 'Under Agreement',
+                photo: 'https://img-cdn.tid.al/o/4858a4b2723b7d0c7d05584ff57701f7b0c54ce3.jpg',
+                children: [
+                  {
+                    id: 12,
+                    name: 'Dorothy Turner',
+                    email: 'dturner@yoyodyne.com',
+                    phone: '555-0108',
+                    dealStatus: 'Closed',
+                    photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+                    children: []
+                  },
+                  {
+                    id: 13,
+                    name: 'Dorothy Turner',
+                    email: 'dturner@yoyodyne.com',
+                    phone: '555-0108',
+                    dealStatus: 'Closed',
+                    photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+                    children: [
+                      {
+                        id: 14,
+                        name: 'Dorothy Turner',
+                        email: 'dturner@yoyodyne.com',
+                        phone: '555-0108',
+                        dealStatus: 'Closed',
+                        photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+                        children: []
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+            {
+              id: 7,
+              name: 'Dorothy Turner',
+              email: 'dturner@yoyodyne.com',
+              phone: '555-0108',
+              dealStatus: 'Closed',
+              photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+              children: []
+            },
+            {
+              id: 8,
+              name: 'Dorothy Turner',
+              email: 'dturner@yoyodyne.com',
+              phone: '555-0108',
+              dealStatus: 'Closed',
+              photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+              children: []
+            },
+            {
+              id: 9,
+              name: 'Dorothy Turner',
+              email: 'dturner@yoyodyne.com',
+              phone: '555-0108',
+              dealStatus: 'Closed',
+              photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+              children: []
+            },
+            {
+              id: 10,
+              name: 'Dorothy Turner',
+              email: 'dturner@yoyodyne.com',
+              phone: '555-0108',
+              dealStatus: 'Closed',
+              photo: 'https://alicepropertymanagement.com/images/temp-profile.jpg',
+              children: []
+            }]
+        }]
+    };
 @Component({
   selector: 'app-referral-lineage',
   templateUrl: './referral-lineage.component.html',
   styleUrls: ['./referral-lineage.component.scss'],
 
 })
-export class ReferralLineageComponent implements OnInit, AfterViewInit{
-  @ViewChild(GraphComponentComponent) gcComponent!: GraphComponentComponent;
+export class ReferralLineageComponent implements OnInit{
   public currentPerson?: BuyerModel;
 
   // eslint-disable-next-line no-useless-constructor
-  constructor(
-    private _injector: Injector,
-    private _resolver: ComponentFactoryResolver,
-    private _appRef: ApplicationRef,
-    private _zone: NgZone
-  ) {}
+  constructor() {}
   ngOnInit(): void {
   }
-
-  ngAfterViewInit() {
-    // run outside Angular so the change detection won't slow down yFiles
-    this._zone.runOutsideAngular(() => {
-      const graphComponent = this.gcComponent.graphComponent;
-      const graph = graphComponent.graph
-
-      graph.nodeDefaults.size = new Size(285, 100)
-      graph.nodeDefaults.style = new NodeComponentStyle(
-        this._injector,
-        this._resolver,
-        this._appRef,
-        this._zone
-      );
-
-      graph.edgeDefaults.style = new PolylineEdgeStyle({
-        stroke: '1px rgb(170, 170, 170)',
-        targetArrow: IArrow.NONE
-      });
-
-      graphComponent.addCurrentItemChangedListener(() => {
-        this.currentPerson = graphComponent.currentItem!.tag;
-      });
-
-      createSampleGraph(graph);
-
-      runLayout(graph);
-
-      var h = new GraphTransformer();
-      h.scaleFactorY = 2;
-
-      graphComponent.fitGraphBounds();
-    });
-  }
-}
-
-function createSampleGraph(graph: IGraph): void {
-  const nodeMap: { [id: number]: INode } = {};
-
-  const parentNodeCheck = true;
-  FlatTree(NODE_DATA, parentNodeCheck, nodeMap, graph);
-
-  EDGE_DATA.forEach(({ from, to }) => {
-    const fromNode = nodeMap[from]
-    const toNode = nodeMap[to]
-    if (fromNode && toNode) {
-      graph.createEdge(fromNode, toNode);
-    }
-  });
-
-}
-
-function runLayout(graph: IGraph): void {
-  const treeLayout = new TreeLayout();
-  const treeReductionStage = new TreeReductionStage();
-  treeReductionStage.nonTreeEdgeRouter = new OrganicEdgeRouter();
-  treeReductionStage.nonTreeEdgeSelectionKey = OrganicEdgeRouter.AFFECTED_EDGES_DP_KEY;
-  treeLayout.defaultNodePlacer =  new DefaultNodePlacer(0, 3, 1, 3, 1, 50, 50, 0);
-  treeLayout.appendStage(treeReductionStage);
-  graph.applyLayout(treeLayout);
-}
-
-function FlatTree(root, parentNodeCheck, nodeMap, graph): void {
-  if(parentNodeCheck){
-    const nodeData = root;
-    nodeMap[nodeData.id] = graph.createNode({
-      tag: new BuyerModel(nodeData)
-    });
-    createPort(graph, nodeMap, nodeData, parentNodeCheck);
-    parentNodeCheck = false;
-  }
-  for (const nodeData of root.children) {
-    nodeMap[nodeData.id] = graph.createNode({
-      tag: new BuyerModel(nodeData)
-    });
-    createPort(graph, nodeMap, nodeData, parentNodeCheck);
-    FlatTree(nodeData, parentNodeCheck, nodeMap, graph);
-  }
-}
-
-function createPort(graph, nodeMap, nodeData, parentNodeCheck): void {
-  const green = '#53E773';
-  const gray = '#B3B7C0';
-  let color = green;
-  if (parentNodeCheck) {
-    addPort(graph, nodeMap, nodeData, green, 142.5, 104 );
-  }
-  else {
-      if (nodeData.children.length === 0) { // Leafs
-        color = nodeData.dealStatus === 'Closed' ? gray : green;
-        addPort(graph, nodeMap, nodeData, color, 142.5, -5 );
-      }
-      else{
-        color = nodeData.dealStatus === 'Closed' ? gray : green;
-        addPort(graph, nodeMap, nodeData, color, 142.5, -5 );
-        addPort(graph, nodeMap, nodeData, color, 142.5, 104 );
-      }
-  }
-}
-
-function addPort(graph, nodeMap, nodeData, color, x, y): void {
-  const parentPortStyle = new NodeStylePortStyleAdapter(
-    new ShapeNodeStyle({fill: color, stroke: 'transparent', }));
-  parentPortStyle.renderSize = new Size(10, 10);
-  graph.addPortAt(nodeMap[nodeData.id], new Point(x, y), parentPortStyle);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import {License} from 'yfiles';
-License.value = {
-  "comment": "0c49103c-818c-4871-a56d-364696990444",
-  "date": "04/05/2021",
-  "distribution": false,
-  "domains": [
-    "*"
-  ],
-  "expires": "06/05/2021",
-  "fileSystemAllowed": true,
-  "licensefileversion": "1.1",
-  "localhost": true,
-  "oobAllowed": true,
-  "package": "complete",
-  "product": "yFiles for HTML",
-  "type": "eval",
-  "version": "2.3",
-  "watermark": "yFiles HTML Evaluation License (expires in ${license-days-remaining} days)",
-  "key": "88860e3a38a73ef9003c6bbfd15164032bba4066"
-};
 
 if (environment.production) {
   enableProdMode();
